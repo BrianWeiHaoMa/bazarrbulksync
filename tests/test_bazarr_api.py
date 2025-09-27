dd408cf63ab759787d523c324f892c47407be744
BrianWeiHaoMa <brianmaytc@gmail.com>
2025-09-26 21:20:05 -0400

    redo everything in a new format

--- tests/test_bazarr_api.py
+++ tests/test_bazarr_api.py
@@ -1,45 +1,57 @@
 import itertools
 from urllib.parse import urljoin
 import json
+import logging
 
 import requests
 import pytest
 
-from . import (
-    base_headers,
-    bazarr_base_url,
-    LARGE_NUMBER
+from sync.bazarr import (
+    BazarrAPI,
+)
+from .config import (
+    BAZARR_API_KEY,
+    BAZARR_BASE_URL,
 )
 
+logging.disable()
+
+base_headers = {
+    "X-API-KEY": BAZARR_API_KEY,
+}
+
+
 def helper_get_response_total(
-        url: str,
+    url: str,
 ) -> int:
     res = requests.get(
         url=url,
         headers=base_headers,
     )
-    
+
     holder = json.loads(res.content)
-    
+
     return holder["total"]
 
 
 def test_bazarr_has_at_least_10_series():
-    url = urljoin(bazarr_base_url, "api/series")
-    assert helper_get_response_total(url) >= 10, \
+    url = urljoin(BAZARR_BASE_URL, "api/series")
+    assert helper_get_response_total(url) >= 10, (
         "bazarr should have >= 10 series for these tests"
-    
-    
+    )
+
+
 def test_bazarr_has_at_least_10_movies():
-    url = urljoin(bazarr_base_url, "api/movies")
-    assert helper_get_response_total(url) >= 10, \
+    url = urljoin(BAZARR_BASE_URL, "api/movies")
+    assert helper_get_response_total(url) >= 10, (
         "bazarr should have >= 10 movies for these tests"
-    
-    
+    )
+
+
 def helper_check_data_total_style_response_structure(
-        url: str,
-        start: int | None,
-        length: int | None,
+    url: str,
+    start: int | None,
+    length: int | None,
 ):
     res = requests.get(
         url=url,
@@ -49,20 +61,20 @@ def helper_check_data_total_style_response_structure(
         },
         headers=base_headers,
     )
-    
+
     holder: dict = json.loads(res.content)
-    
+
     keys = holder.keys()
     assert keys == {"data", "total"}
-    
-    assert type(holder["data"]) == list
-    assert type(holder["total"]) == int
-    
+
+    assert type(holder["data"]) is list
+    assert type(holder["total"]) is int
+
     if start is None or start < 0:
         sstart = 0
     else:
         sstart = start
-        
+
     total = holder["total"]
     if length is None or length <= 0:
         assert len(holder["data"]) == total
@@ -76,12 +88,12 @@ START_LENGTH_STYLE_POSSIBILITIES = (
     -3,
     0,
     5,
-    LARGE_NUMBER,
+    BazarrAPI.LARGE_NUMBER,
 )
 
 start_length_style_inputs = list(
     itertools.product(
-        START_LENGTH_STYLE_POSSIBILITIES, 
+        START_LENGTH_STYLE_POSSIBILITIES,
         START_LENGTH_STYLE_POSSIBILITIES,
     )
 )
@@ -92,22 +104,22 @@ start_length_style_inputs = list(
     start_length_style_inputs,
 )
 def test_get_series_response_structure(start, length):
-    url = urljoin(bazarr_base_url, "api/series")
-    
+    url = urljoin(BAZARR_BASE_URL, "api/series")
+
     helper_check_data_total_style_response_structure(
         url=url,
         start=start,
         length=length,
     )
-    
-    
+
+
 @pytest.mark.parametrize(
     "start, length",
     start_length_style_inputs,
 )
 def test_get_movies_response_structure(start, length):
-    url = urljoin(bazarr_base_url, "api/movies")
-    
+    url = urljoin(BAZARR_BASE_URL, "api/movies")
+
     helper_check_data_total_style_response_structure(
         url=url,
         start=start,
